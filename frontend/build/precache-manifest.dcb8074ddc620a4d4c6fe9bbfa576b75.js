bc2bbfaedd9a3de3aa080d8c0a9baca71c4d07fa
LarryLeones <73570799+SeneoL06@users.noreply.github.com>
2020-11-19 12:57:27 +0800

    Add files via upload

---
+++ frontend/build/precache-manifest.dcb8074ddc620a4d4c6fe9bbfa576b75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f11c23fdacfb7a157a632dafc53f5d6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d6fe63dba28a5dada8b",
+    "url": "/static/css/main.e915e0db.chunk.css"
+  },
+  {
+    "revision": "7f9f17edf8828e3a02a8",
+    "url": "/static/js/2.e2029a90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e2029a90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d6fe63dba28a5dada8b",
+    "url": "/static/js/main.dfd5ef43.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
